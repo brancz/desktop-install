44e9f4293fa04b733c6d0f038094e2103a9f1f6d
flower-pot <flowerpot@pondati.net>
2013-10-25 11:40:37 +0200

    Added dependencies

--- rails.sh
+++ rails.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt-get -y install gawk libgdbm-dev pkg-config libffi-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion python
+sudo apt-get -y install gawk libgdbm-dev pkg-config libffi-dev build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion python postgresql postgresql-contrib libpq-dev nodejs
 curl -L https://get.rvm.io | bash -s stable --rails
 source ~/.rvm/scripts/rvm
