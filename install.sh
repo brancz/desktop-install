5c91c2a897ff8d2a65daf3ba10ad7ec393c2ddb4
flower-pot <flowerpot@pondati.net>
2014-04-18 16:22:01 +0200

    update vagrant version

--- install.sh
+++ install.sh
@@ -78,8 +78,8 @@ function install_selected() {
   then
     echo_headline "INSTALLING VAGRANT"
 
-    LATEST_VAGRANT_32="https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.0_i686.deb"
-    LATEST_VAGRANT_64="https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.0_x86_64.deb"
+    LATEST_VAGRANT_32="https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.3_i686.deb"
+    LATEST_VAGRANT_64="https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.3_x86_64.deb"
 
     ## Vagrant
     apt-get install -y dpkg virtualbox
