c0f16ba0962339dfd8f36a41ba55ece1501b1207
Frederic Branczyk <frederic.branczyk@sap.com>
2014-07-31 11:51:29 -0700

    first version of osx script

---
+++ osx.sh
@@ -0,0 +1,31 @@
+if [[ $USER != "root" ]]; then
+  echo "You need to run this as root."
+  exit 1
+fi
+
+homedir=`eval echo ~$SUDO_USER`
+
+echo "INSTALLING ZSH, AND DOTFILES"
+
+echo "Check if homebrew is installed"
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    # https://github.com/mxcl/homebrew/wiki/installation
+    echo "Homebrew is not installed, I will take care of that"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+else
+    brew update
+fi
+
+brew install zsh
+
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sudo -u "${SUDO_USER}" -H sh
+curl -L https://raw.github.com/maksimr/dotfiles/master/gnome-terminal-themes/molokai.sh | sudo -u "${SUDO_USER}" -H sh
+
+rm "${homedir}/.zshrc"
+sudo -u "${SUDO_USER}" -H git clone https://github.com/flower-pot/dotfiles.git "${homedir}/dotfiles"
+ln -s "${homedir}/dotfiles/zsh/flopska.zsh-theme" "${homedir}/.oh-my-zsh/themes/flopska.zsh-theme"
+ln -s "${homedir}/dotfiles/zsh/zshrc" "${homedir}/.zshrc"
+
+chsh $SUDO_USER -s /bin/zsh
