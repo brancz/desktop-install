a2dfc80d76baf810129498c91a9e5bf14987eba2
Frederic Branczyk <fbranczyk@gmail.com>
2014-07-31 15:39:59 -0700

    fix some errors

--- osx.sh
+++ osx.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [[ $USER != "root" ]]; then
   echo "You need to run this as root."
   exit 1
@@ -13,12 +15,14 @@ if [[ $? != 0 ]] ; then
     # Install Homebrew
     # https://github.com/mxcl/homebrew/wiki/installation
     echo "Homebrew is not installed, I will take care of that"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    echo "${SUDO_USER}"
+    echo "curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install | ruby"
+    su -l "${SUDO_USER}" -c "curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install | ruby"
 else
     brew update
 fi
 
-brew install zsh
+brew install zsh git
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sudo -u "${SUDO_USER}" -H sh
 curl -L https://raw.github.com/maksimr/dotfiles/master/gnome-terminal-themes/molokai.sh | sudo -u "${SUDO_USER}" -H sh
