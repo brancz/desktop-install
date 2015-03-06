f2532dc10330230038fb3acb2a7c56df624b991f
Frederic Branczyk <fbranczyk@gmail.com>
2015-03-06 10:50:50 +0100

    force yes for docker install

--- install.sh
+++ install.sh
@@ -61,7 +61,7 @@ function install_docker() {
   apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
   sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
   apt-get update
-  apt-get install lxc-docker
+  apt-get install -q -y lxc-docker
   curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
