41dda10da153adc56d6e161cb0d64485d87de96e
Frederic Branczyk <fbranczyk@gmail.com>
2015-05-29 18:31:17 +0200

    update docker-compose version

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ function install_docker() {
   sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
   apt-get update
   apt-get install -q -y lxc-docker
-  curl -L https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
 
