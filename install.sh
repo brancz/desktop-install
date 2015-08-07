3cfee71dae618edc29b59c2dae236655949f5d61
flower-pot <fbranczyk@gmail.com>
2015-08-07 17:09:22 +0200

    install docker using their script

--- install.sh
+++ install.sh
@@ -58,10 +58,7 @@ function install_ruby() {
 function install_docker() {
   echo_headline "INSTALLING DOCKER AND DOCKER-COMPOSE"
 
-  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-  sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-  apt-get update
-  apt-get install -q -y lxc-docker
+  wget -qO- https://get.docker.com/ | sh
   curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
