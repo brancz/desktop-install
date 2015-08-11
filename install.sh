5a0f87c686c000a386700c65bcecc64c488224b0
Frederic Branczyk <fbranczyk@gmail.com>
2015-08-11 21:56:14 +0200

    new docker-compose version

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ function install_docker() {
   echo_headline "INSTALLING DOCKER AND DOCKER-COMPOSE"
 
   wget -qO- https://get.docker.com/ | sh
-  curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 }
 
